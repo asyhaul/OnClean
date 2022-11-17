412a356a4215095bf7d47cf0d53e96112cc45483
Aisyah Aulia Zahra <aisyahauliazahra3909@gmail.com>
2022-11-17 18:03:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/onclean/onclean2.java
@@ -0,0 +1,14 @@
+package com.example.onclean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class onclean2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onclean2);
+    }
+}
---
+++ app/src/main/java/com/example/onclean/onclean4.java
@@ -0,0 +1,14 @@
+package com.example.onclean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class onclean4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onclean4);
+    }
+}
